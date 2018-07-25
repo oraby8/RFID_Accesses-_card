4d289feaffcc01ebe21f49f09523d08aba30995a
oraby8 <oraby888@gmail.com>
2018-07-25 14:51:17 +0200

    Add files via upload

---
+++ RFID_Acceses_card.ino
@@ -0,0 +1,17 @@
+int a=0;
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop() {
+  a=analogRead(0)*50;
+  Serial.println(a);
+ // if (a>600){
+   // Serial.println("HIGH");
+    
+  //  }
+  //else{
+    //Serial.println("LOW");
+    //}
+  delay(1000);
+}
